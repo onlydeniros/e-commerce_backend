45dbf90920d6f729865b9dd6b1640dc1587484a1
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-20 20:26:44 -0400

    route fixed

--- models/index.js
+++ models/index.js
@@ -7,24 +7,21 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-  onUpdate: Category
-})
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'tag_id'
-})
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
-})
+});
 module.exports = {
   Product,
   Category,
